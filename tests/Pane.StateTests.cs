46044b91b0e4ec4dc9ea5a57d9e945283f58ad6c
Lukasz Patalas <lpatalas@gmail.com>
2019-06-22 12:54:42 +0200

    Add test for changing pane state

--- tests/Pane.StateTests.cs
+++ tests/Pane.StateTests.cs
@@ -4,7 +4,7 @@ using Xunit;
 
 namespace PoshCommander.Tests
 {
-    public class PaneInitialStateTests
+    public class PaneStateTests
     {
         [Fact]
         public void When_its_created_it_should_correctly_set_initial_view_state()
@@ -28,6 +28,27 @@ namespace PoshCommander.Tests
             view.PaneState.Should().Be(paneState);
             view.StatusText.Should().Be($"Files: {fileCount}, Directories: {directoryCount}");
             view.Title.Should().Be(directory);
+        }
+
+        [Theory]
+        [InlineData(PaneState.Inactive, PaneState.Active)]
+        [InlineData(PaneState.Active, PaneState.Inactive)]
+        public void When_pane_is_activated_or_deactivated_it_should_correctly_set_view_state(
+            PaneState originalState,
+            PaneState newState)
+        {
+            // Arrange
+            var directory = @"C:\Windows\System32";
+            var fileSystem = StubFileSystem.FromItemCount(5);
+            var view = new FakePaneView();
+
+            var pane = new Pane(directory, fileSystem, originalState, view);
+
+            // Act
+            pane.State = newState;
+
+            // Assert
+            view.PaneState.Should().Be(newState);
         }
     }
 }
