badd3c0f268131e63351dd2e562b0f252e2d695b
Lukasz Patalas <lpatalas@gmail.com>
2019-08-16 20:42:29 +0200

    Add `CreateDirectoryCommand` tests

---
+++ tests/TestDoubles/FakeFileSystemPane.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using PoshCommander.UI;
+
+namespace PoshCommander.Tests.TestDoubles
+{
+    public class FakeFileSystemPane : IFileSystemPane
+    {
+        private readonly List<string> _createdDirectories = new List<string>();
+        public IReadOnlyList<string> CreatedDirectories => _createdDirectories;
+
+        public IPaneView View { get; } = new FakePaneView();
+
+        public void CreateDirectory(string name)
+        {
+            _createdDirectories.Add(name);
+        }
+    }
+}
