0edcdd2320f6d598b55768696b165f1c58de4726
Lukasz Patalas <lpatalas@gmail.com>
2019-09-08 01:32:36 +0200

    Change test module organization

---
+++ fsharp/PoshCommander.Tests/Input.fs
@@ -0,0 +1,103 @@
+﻿module PoshCommander.Tests.Input
+
+open PoshCommander.UI
+open System
+open NUnit.Framework
+open Swensen.Unquote
+
+let charToConsoleKey c =
+    enum (int (Char.ToUpperInvariant(c)))
+
+let charToConsoleKeyInfo c =
+    new ConsoleKeyInfo(c, charToConsoleKey c, false, false, false)
+
+let createInputSequence input =
+    input |> Seq.map charToConsoleKeyInfo
+
+let readTestInput inputString =
+    let pressedKeys = createInputSequence inputString
+    Input.readInput (fun _ -> true) ignore pressedKeys
+
+let shouldBeReadAsOption expectedResult inputString =
+    let result = readTestInput inputString
+    test <@ result = expectedResult @>
+
+let shouldBeReadAs expectedResult =
+    shouldBeReadAsOption (Some expectedResult)
+
+let shouldBeReadAsNone =
+    shouldBeReadAsOption None
+
+module readInput =
+    [<Test>]
+    let ``Should return empty string when only Enter is pressed``() =
+        "\r" |> shouldBeReadAs ""
+
+    [<Test>]
+    let ``Should concatenate all pressed keys until Enter is pressed``() =
+        "Input\r" |> shouldBeReadAs "Input"
+
+    [<Test>]
+    let ``Should stop reading input immediately after Enter is pressed``() =
+        "In\rput" |> shouldBeReadAs "In"
+
+    [<Test>]
+    let ``Should erase last character when backspace is pressed``() =
+        "In\bpu\bt\r" |> shouldBeReadAs "Ipt"
+
+    [<Test>]
+    let ``Should return empty string when all characters are erased``() =
+        "Input\b\b\b\b\b\r" |> shouldBeReadAs ""
+
+    [<Test>]
+    let ``Should skip all characters for which predicate returns false``() =
+        let predicate c =
+            c = 'A' || c = 'C' || c = 'E'
+
+        let inputSequence = createInputSequence "ABCDEF\r"
+        let result = Input.readInput predicate ignore inputSequence
+        test <@ result = Some "ACE" @>
+
+    [<TestCase("\x1b", TestName = "{m}(<ESC>)")>]
+    [<TestCase("Inp\x1but", TestName = "{m}(Inp<ESC>ut)")>]
+    let ``Should return None when Escape is pressed`` (input: String) =
+        input |> shouldBeReadAsNone
+
+    [<TestCase('\r', TestName = "{m}(Enter)")>]
+    [<TestCase('\u001b', TestName = "{m}(Escape)")>]
+    let ``Should stop reading input after Enter or Escape is pressed`` (breakCharacter: char) =
+        let chars = [| 'A'; 'B'; breakCharacter; 'X'; 'Y' |]
+        let mutable charIndex = 0
+
+        let inputSequence = seq {
+            while charIndex < chars.Length do
+                let c = chars.[charIndex]
+                charIndex <- charIndex + 1
+                yield charToConsoleKeyInfo c
+        }
+
+        Input.readInput (fun _ -> true) ignore inputSequence |> ignore
+
+        let nextChar = (Seq.head inputSequence).KeyChar
+        test <@ nextChar = 'X' @>
+
+    [<Test>]
+    let ``Should execute callback after each typed character``() =
+        let mutable inputHistory = []
+        let callback input =
+            inputHistory <- inputHistory @ [input]
+
+        let inputSequence = createInputSequence "Inp\but\r"
+        Input.readInput (fun _ -> true) callback inputSequence |> ignore
+
+        let expectedHistory = [
+            ""
+            "I"
+            "In"
+            "Inp"
+            "In"
+            "Inu"
+            "Inut"
+        ]
+
+        test <@ inputHistory = expectedHistory @>
--- fsharp/PoshCommander.Tests/Theme.fs
+++ fsharp/PoshCommander.Tests/Theme.fs
@@ -24,19 +24,19 @@ let defaultIcon = createWhiteIcon '0'
 
 module getFileIcon =
     [<Test>]
-    let ``should return defaultIcon when file name does not match any pattern``() =
+    let ``Should return defaultIcon when file name does not match any pattern``() =
         let patterns = createPatterns [ @"\.txt" ]
         let result = getFileIcon defaultIcon patterns "file.bmp"
         test <@ result = defaultIcon @>
 
     [<Test>]
-    let ``should return first match if file name matches multple patterns``() =
+    let ``Should return first match if file name matches multple patterns``() =
         let patterns = createPatterns [ @"\.tx"; @"\.txt" ]
         let result = getFileIcon defaultIcon patterns "file.txt"
         test <@ result = patterns.[0].Icon @>
     
     [<Test>]
-    let ``should skip patterns which does not match file name``() =
+    let ``Should skip patterns which does not match file name``() =
         let patterns = createPatterns [ @"\.bmp"; @"\.png"; @"\.txt" ]
         let result = getFileIcon defaultIcon patterns "file.txt"
         test <@ result = patterns.[2].Icon @>
