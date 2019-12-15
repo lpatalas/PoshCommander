6606d70a2f612604ace57717dedba153e27e37be
Lukasz Patalas <lpatalas@gmail.com>
2019-12-15 21:43:46 +0100

    Add `build.cmd` batch file

---
+++ build.cmd
@@ -0,0 +1,7 @@
+@echo off
+pwsh.exe -Version 2>NUL >NUL || (
+    echo "pwsh.exe not found. Install PowerShell Core from https://github.com/PowerShell/PowerShell"
+    exit /B
+)
+
+pwsh.exe -NoProfile -ExecutionPolicy RemoteSigned -File "%~dp0scripts\Invoke-Build.ps1"
